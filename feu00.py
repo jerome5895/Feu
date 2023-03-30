fd82eb8e12d2a7572bbe7c624c0836d033f1570b
Jérôme Theytaz <jerome.theytaz@gmail.com>
2023-03-30 10:12:52 -0400

    Added errors function, feu00.py

--- feu00.py
+++ feu00.py
@@ -1,9 +1,7 @@
 import sys
 
-C = int(sys.argv[1])
-L = int(sys.argv[2])
-
-def rectangle():
+# Function to print out a rectangle
+def rectangle(C, L):
     for i in range(1, L+1):
         for j in range(1, C+1):
             if i == 1 or i == L or j == 1 or j == C:
@@ -12,4 +10,18 @@ def rectangle():
                 print("  ", end="")
         print()
 
-rectangle()
+# Function to manage value and index errors
+def try_except():
+    try:
+        C = int(sys.argv[1])
+        L = int(sys.argv[2])
+    except (ValueError, IndexError):
+        print("Invalid input. Please provide two numbers.")
+        sys.exit()
+    return C, L
+
+# Resolution
+C, L = try_except()
+
+# Print out result
+rectangle(C, L)
