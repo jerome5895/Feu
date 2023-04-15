3d09013f54c1ed79eda2464aa358b3ca86dcadd7
Jérôme Theytaz <jerome.theytaz@gmail.com>
2023-04-14 20:25:53 -0400

    Added new feu01.py

--- feu01.py
+++ feu01.py
@@ -1,13 +1,45 @@
-import sys
+# Définition de la fonction qui évalue une expression entre parenthèses
+def eval_parentheses(tokens):
+    stack = []
+    for token in tokens:
+        if token == '(':
+            stack.append(token)
+        elif token == ')':
+            expr = []
+            while stack[-1] != '(':
+                expr.append(stack.pop())
+            stack.pop()
+            stack.append(str(eval_expr(expr[::-1])))
+        else:
+            stack.append(token)
+    return eval_expr(stack)
 
-def evaluate(expression):
-    try:
-        return eval(expression)
-    except(SyntaxError, ZeroDivisionError, NameError):
-        print("Invalid input. The arithmetic expression is false.")
-        exit()
+# Définition de la fonction qui évalue une expression
+def eval_expr(tokens):
+    operators = ['+', '-', '*', '/', '%']
+    for operator in operators:
+        for i in range(len(tokens)):
+            if tokens[i] == operator:
+                left = eval_expr(tokens[:i])
+                right = eval_expr(tokens[i+1:])
+                if operator == '+':
+                    return left + right
+                elif operator == '-':
+                    return left - right
+                elif operator == '*':
+                    return left * right
+                elif operator == '/':
+                    return left / right
+                elif operator == '%':
+                    return left % right
+    if len(tokens) == 1:
+        return int(tokens[0])
+    else:
+        return eval_parentheses(tokens)
+    
+# Programme principal
+if __name__ == '__main__':
+    import sys
+    tokens = sys.argv[1].split()
+    print(eval_expr(tokens))
 
-expression = sys.argv[1]
-
-result = evaluate(expression)
-print("The result of your arithmetic expresssion is: ", result)
