151b11d19ce636ea60c20c0614eef4b6aa879167
Jérôme Theytaz <jerome.theytaz@gmail.com>
2023-04-19 18:55:15 -0400

    Added new feu01.1.py

--- feu01.1.py
+++ feu01.1.py
@@ -1,26 +1,10 @@
-mod = 1000000007
+import sys
 
-# Function to evaluate arithmetic expression
-def ValOfTheExpression(n):
-    global mod
+expression = sys.argv[1]
 
-    factorial = [0 for i in range(n + 1)]
-    
-    factorial[0] = 1
-    factorial[1] = 1
-
-    for i in range(2, n + 1, 1):
-        factorial[i] = ((factorial[i - 1] % mod) * (i % mod)) % mod
-
-    dp = [0 for i in range(n + 1)]
-    dp[1] = 1
-
-    for i in range(2, n + 1, 1):
-        dp[i] = ((dp[i - 1] % mod) * (factorial[i] % mod)) % mod
-
-    return dp[n]
-
-# Main program
-if __name__ == '__main__':
-    n = 4
-    print(ValOfTheExpression(n))
+def evaluate(expr):
+    operators = ['+', '-', '*', '/', '%']
+    for operator in operators:
+        for i in range(expr + 1):
+            if i == operator:
+                
