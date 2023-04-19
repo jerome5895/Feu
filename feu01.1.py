18442571c04bc739d8d92e365cd0bd1bfafd4404
Jérôme Theytaz <jerome.theytaz@gmail.com>
2023-04-19 19:14:19 -0400

    Finished evaluate(expression) function, feu01.1.py

--- feu01.1.py
+++ feu01.1.py
@@ -1,10 +1,26 @@
-import sys
-
-expression = sys.argv[1]
-
-def evaluate(expr):
+# Function to evaluate an arithmetic expression
+def evaluate(expression):
     operators = ['+', '-', '*', '/', '%']
     for operator in operators:
-        for i in range(expr + 1):
-            if i == operator:
-                
+        for i in range(len(expression)):
+            if expression[i] == operator:
+                left = evaluate(expression[:i])
+                right = evaluate(expression[i+1:])
+                if operator == '+':
+                    return left + right
+                elif operator == '-':
+                    return left - right
+                elif operator == '*':
+                    return left * right
+                elif operator == '/':
+                    return left / right
+                elif operator == '%':
+                    return left % right
+    if len(expression) == 1:
+        return int(expression[0])
+    
+# Main program
+if __name__ == '__main__':
+    import sys
+    expression = sys.argv[1].split()
+    print(evaluate(expression))
