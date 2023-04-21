e36f0aecacca5c663603f3836f75db7d567c3d90
Jérôme Theytaz <jerome.theytaz@gmail.com>
2023-04-20 20:09:29 -0400

    Translate to english, feu01.1.py

--- feu01.1.py
+++ feu01.1.py
@@ -1,51 +1,51 @@
 import sys
 
 # Function to evaluate an arithmetic expression
-def evaluer_expression(expression):
+def evaluate_expression(expression):
 
-    operandes = []
-    operateurs = []
+    operands = []
+    operators = []
     
-    def calculer_operation():
-        operateur = operateurs.pop()
-        operande2 = operandes.pop()
-        operande1 = operandes.pop()
+    def calculate_operation():
+        operator = operators.pop()
+        operand2 = operands.pop()
+        operand1 = operands.pop()
     
-        if operateur == '+':
-            operandes.append(operande1 + operande2)
-        elif operateur == '-':
-            operandes.append(operande1 - operande2)
-        elif operateur == '*':
-            operandes.append(operande1 * operande2)
-        elif operateur == '/':
-            operandes.append(operande1 / operande2)
-        elif operateur == '%':
-            operandes.append(operande1 % operande2)
+        if operator == '+':
+            operands.append(operand1 + operand2)
+        elif operator == '-':
+            operands.append(operand1 - operand2)
+        elif operator == '*':
+            operands.append(operand1 * operand2)
+        elif operator == '/':
+            operands.append(operand1 / operand2)
+        elif operator == '%':
+            operands.append(operand1 % operand2)
     
-    for caractere in expression:
-        if caractere.isdigit():
-            operandes.append(int(caractere))
-        elif caractere in ['+', '-', '*', '/', '%']:
-            while operateurs and operateurs[-1] in ['*', '/']:
-                calculer_operation()
-            operateurs.append(caractere)
-        elif caractere == '(':
-            operateurs.append(caractere)
-        elif caractere == ')':
-            while operateurs[-1] != '(':
-                calculer_operation()
-            operateurs.pop()
-    while operateurs:
-        calculer_operation()
+    for character in expression:
+        if character.isdigit():
+            operands.append(int(character))
+        elif character in ['+', '-', '*', '/', '%']:
+            while operators and operators[-1] in ['*', '/']:
+                calculate_operation()
+            operators.append(character)
+        elif character == '(':
+            operators.append(character)
+        elif character == ')':
+            while operators[-1] != '(':
+                calculate_operation()
+            operators.pop()
+    while operators:
+        calculate_operation()
     
-    return operandes[0]
+    return operands[0]
 
 
 # Globales variables
 expression = sys.argv[1]
 
 # Resolution
-resultat = evaluer_expression(expression)
+result = evaluate_expression(expression)
 
 # Print out result
-print(resultat)
+print(result)
