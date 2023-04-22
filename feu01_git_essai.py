60dc42be332cbfb59c0d5725ffa02f2fd98216ce
Jérôme Theytaz <jerome.theytaz@gmail.com>
2023-04-21 22:43:26 -0400

    Added feu01_git_essai.py

---
+++ feu01_git_essai.py
@@ -0,0 +1,40 @@
+import sys
+
+# Function to evaluate an arithmetic expression
+def evaluate_expression(expression, calculate_operation):
+
+    operands = []
+    operators = []
+
+    for character in expression:
+        if character.isdigit():
+            operands.append(int(character))
+        elif character in ['+', '-']:
+            calculate_operation()
+            operators.append(character)
+
+    return operands[0]
+
+# Function to calculate binary operations
+def calculate_operation(operands, operators):
+    operator = operators.pop()
+    operand2 = operands.pop()
+    operand1 = operand2.pop()
+
+    if operator == '+':
+        operands.append(operand1 + operand2)
+    elif operator == '-':
+        operands.append(operand1 - operand2)
+    elif operator == '*':
+        operands.append(operand1 * operand2)
+    elif operator == '/':
+        operands.append(operand1 / operand2)
+    elif operator == '%':
+        operands.append(operand1 % operand2)
+
+
+expression = sys.argv[1]
+
+result = evaluate_expression(expression, calculate_operation)
+
+print(result)
