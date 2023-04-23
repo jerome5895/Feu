42075fa38e7e1e65914ec612e8fb498382b4b8be
Jérôme Theytaz <jerome.theytaz@gmail.com>
2023-04-22 22:23:25 -0400

    Edited evaluate_expression function, feu01_git_essai.py

--- feu01_git_essai.py
+++ feu01_git_essai.py
@@ -1,5 +1,3 @@
-import sys
-
 # Function to evaluate an arithmetic expression
 def evaluate_expression(expression):
 
@@ -9,7 +7,7 @@ def evaluate_expression(expression):
     def calculate_operation():
         operator = operators.pop()
         operand2 = operands.pop()
-        operand1 = operand2.pop()
+        operand1 = operands.pop()
 
         if operator == '+':
             operands.append(operand1 + operand2)
@@ -22,17 +20,30 @@ def evaluate_expression(expression):
         elif operator == '%':
             operands.append(operand1 % operand2)
 
+    precedence = {'+': 1, '-': 1, '*': 2, '/': 2, '%': 2}
+
     for character in expression:
         if character.isdigit():
             operands.append(int(character))
-        elif character in ['+', '-']:
-            calculate_operation()
+        elif character in ['+', '-', '*', '/', '%']:
+            while operators and operators[-1] != '(' and precedence[operators[-1]] >= precedence[character]:
+                calculate_operation()
             operators.append(character)
+        elif character == '(':
+            operators.append(character)
+        elif character == ')':
+            while operators[-1] != '(':
+                calculate_operation()
+    while operators:
+        calculate_operation()
 
     return operands[0]
 
+
 expression = sys.argv[1]
 
+
 result = evaluate_expression(expression)
+
 
 print(result)
